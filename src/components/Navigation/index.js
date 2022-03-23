c953ee5922f916619d07259fd2bf3df45558c4ec
Ross Westwater <ross.westwater@gmail.com>
2022-03-23 17:15:16 -0600

    added README

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -10,8 +10,8 @@ function Nav(props) {
   <nav>
     <ul className='flex-row'>
       <li style={props.page === 'about' ? {background: "rgb(132, 169, 192)"}: {}} name='about' onClick={clickHandler}>About me</li>
-      <li style={props.page === 'contact' ? {background: "rgb(132, 169, 192)"}: {}} name='contact' onClick={clickHandler}>Contact</li>
       <li style={props.page === 'portfolio' ? {background: "rgb(132, 169, 192)"}: {}} name='portfolio' onClick={clickHandler}>Portfolio</li>
+      <li style={props.page === 'contact' ? {background: "rgb(132, 169, 192)"}: {}} name='contact' onClick={clickHandler}>Contact</li>
       <li style={props.page === 'resume' ? {background: "rgb(132, 169, 192)"}: {}} name='resume' onClick={clickHandler}>Resume</li>
     </ul>
   </nav>
